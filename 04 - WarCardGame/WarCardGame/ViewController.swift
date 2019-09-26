47d631336d1fde9d74d06fefed61a8f63768486f
Peter McCready <petermccready2@gmail.com>
2019-09-26 17:09:54 +0100

    keep score

--- 04 - WarCardGame/WarCardGame/ViewController.swift
+++ 04 - WarCardGame/WarCardGame/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftScoreLabel: UILabel!
     @IBOutlet weak var rightScoreLabel: UILabel!
 
+    var leftScore = 0
+    var rightScore = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
     @IBAction func dealTapped(_ sender: Any) {
         let leftNumber = Int.random(in: 2...14)
@@ -26,5 +28,19 @@ class ViewController: UIViewController {
 
         leftImageView.image = UIImage(named: "card\(leftNumber)")
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+
+        if leftNumber > rightNumber {
+            // left side wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            // right side wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+            // draw
+        }
     }
 }
